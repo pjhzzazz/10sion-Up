45aca0db5a6ebd655d840358f82feeb38d7b932f
LJH-0131 <lyk84210@naver.com>
2025-05-15 02:01:35 +0900

    스테이지3 키블록 오류수정 및 키&블록 완료

--- Assets/Scripts/ObjectScript/Other/PickupableKeyDoor.cs
+++ Assets/Scripts/ObjectScript/Other/PickupableKeyDoor.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class PickupableKeyDoor : MonoBehaviour
 {
-    [SerializeField] GameObject spriteAndCollider;
+    private GameObject spriteAndCollider;
+
+    void Awake()
+    {
+        // "SpriteAndCollider" 라는 이름의 자식만 찾아서 캐싱
+        spriteAndCollider = transform.Find("lock_yellow")?.gameObject;
+        if (spriteAndCollider == null)
+            Debug.LogError("자식 'lock_yellow' 를 찾을 수 없습니다.");
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("KeyTrigger"))
-        {
+        if (collision.CompareTag("KeyTrigger") && spriteAndCollider != null)
             spriteAndCollider.SetActive(false);
-        }
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("KeyTrigger"))
-        {
+        if (collision.CompareTag("KeyTrigger") && spriteAndCollider != null)
             spriteAndCollider.SetActive(true);
-        }
     }
 }
