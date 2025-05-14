169e076c2220604a5fb931da93c25a0fcdda2251
pjhzzazz <pjhzzazzz@naver.com>
2025-05-15 02:08:14 +0900

    edit

--- Assets/Scripts/Obstacles/ProjectTile/RepeatSnowBall.cs
+++ Assets/Scripts/Obstacles/ProjectTile/RepeatSnowBall.cs
@@ -12,13 +12,6 @@ public class RepeatSnowBall : MonoBehaviour
         Destroy(gameObject, lifeSpan);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("RedPlayer") || collision.gameObject.CompareTag("BluePlayer"))
